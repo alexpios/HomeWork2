f37e3e0d6a4ec3dac322a2efa563271ceebd84b9
alexpios <kuchinsky213@gmail.com>
2023-07-24 22:20:54 +0300

    main screen

--- HomeWork2/ViewController.swift
+++ HomeWork2/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redNumber: UILabel!
+    
+    @IBOutlet var blueNumber: UILabel!
+    @IBOutlet var greenNumber: UILabel!
+ 
+    @IBOutlet var viewColor: UIView!
+    
+    @IBOutlet var sliderRed: UISlider!
+    
+    @IBOutlet var sliderGreen: UISlider!
+    
+    @IBOutlet var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        sliderRed.minimumValue = 0
+        sliderRed.maximumValue = 255
+        sliderRed.minimumTrackTintColor = .red
+        sliderBlue.minimumValue = 0
+        sliderBlue.maximumValue = 255
+        sliderBlue.minimumTrackTintColor = .blue
+        sliderGreen.minimumValue = 0
+        sliderGreen.maximumValue = 255
+        sliderGreen.minimumTrackTintColor = .green
+        
+        
     }
 
-
+    @IBAction func redChange(_ sender: Any) {
+        redNumber.text = String(Int(sliderRed.value))
+        
+    }
+    
 }
 
